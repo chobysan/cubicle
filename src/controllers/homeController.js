1d7c86ed13280fe3e6e4bd48bb32ba5f92a872ad
Nikola Chobanov <chobbysan@gmail.com>
2023-02-10 16:22:48 +0200

    show cubes from db in homepage

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,12 @@
-const db = require('../db.json');
+//const db = require('../db.json');
+const Cube = require('../models/cube')
 
-exports.getHomePage = (req, res) => {
+exports.getHomePage = async (req, res) => {
 	const { search, from: difficultyFrom, to: difficultyTo } = req.query;
 
-	let cubes = db.cubes;
+	let cubes = await Cube.find().lean();
 
+	// TODO: Use db filtration instead of memory filtering
 	if (search) {
 		cubes = cubes.filter((cube) => cube.name.toLowerCase().includes(search.toLowerCase()));
 	}
