715d18230330ce3c100d320eda68fa07aa184c5c
Nikola Chobanov <chobbysan@gmail.com>
2023-02-11 16:58:24 +0200

    Add accessory page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/cube');
+const Accessory = require('../models/Accessory');
 
 exports.getCreateCube = (req, res) => {
 	res.render('create');
@@ -6,19 +7,21 @@ exports.getCreateCube = (req, res) => {
 
 exports.postCreateCube = async (req, res) => {
 	const { name, description, imageUrl, difficultyLevel } = req.body;
-
 	const cube = new Cube({ name, description, imageUrl, difficultyLevel });
-
 	await cube.save();
-
 	res.redirect('/');
 };
 
 exports.getDetails = async (req, res) => {
 	const cube = await Cube.findById(req.params.cubeId).lean();
-
 	if (!cube) {
 		return res.redirect('/404');
 	}
 	res.render('details', { cube });
+};
+
+exports.getAttachAccessory = async (req, res) => {
+	const cube = await Cube.findById(req.params.cubeId).lean();
+	const accessories = await Accessory.find().lean();
+	res.render('cube/attach', { cube, accessories });
 };
